fb736062952b02d7410143897e6b17f8f8278fdc
ivon@gmail.com <aingivon@gmail.com>
2023-07-07 02:46:49 +0200

    cambios de login terminado

--- html/resources/views/auth/login-controller.blade.php
+++ html/resources/views/auth/login-controller.blade.php
@@ -1,45 +1,56 @@
-@extends('layouts.app')
 
-@section('content')
-    <div class="flex justify-center items-center h-screen">
-        <div class="w-full max-w-sm">
-            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('login') }}">
-                @csrf
 
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                        Email
-                    </label>
-                    <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="email" type="email" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
-                </div>
+ @extends('layouts.app')
 
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                        Contraseña
-                    </label>
-                    <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password" type="password" placeholder="Contraseña" name="password" required>
-                    @if ($errors->has('email'))
-                        <p class="text-red-500 text-xs italic">{{ $errors->first('email') }}</p>
-                    @endif
-                </div>
+ @section('content')
+     <div class="flex justify-center items-center min-h-screen">
+         <div class="w-96 max-w-sm">
+             <div class="text-black text-3xl font-bold -mt-8 my-8 mx-8 ms-8 me-8 "><br> Iniciar sesión</div>
+ 
+             <form class="bg-white shadow-md rounded px-8 pt-6 pb-9 mb-4" method="POST" action="{{ route('login') }}">
+                 @csrf
+                 <!-- Resto del formulario -->
+ 
+                 <div class="mb-4">
+                     <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                         Correo electrónico
+                     </label>
+                     <input
+                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                         id="email" type="email" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
+                     <!-- Campos de entrada -->
+                 </div>
+ 
+                 <div class="mb-6">
+                     <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                         Contraseña
+                     </label>
+                     <input
+                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                         id="password" type="password" placeholder="Contraseña" name="password" required>
+                     @if ($errors->has('email'))
+                         <p class="text-red-500 text-xs italic">{{ $errors->first('email') }}</p>
+                     @endif
+                     <!-- Campos de entrada -->
+                 </div>
+ 
+                 <div class="flex flex-col items-center justify-between mt-4">
+                     <button class="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
+                         Acceder
+                     </button>
+                     <a class="text-red-500 text-opacity-70 text-[14px] font-normal mt-2">
+                         ¿Olvidaste la contraseña?
+                     </a>
+                     <a class="text-red-500 text-opacity-70 text-[14px] font-normal mt-2" href="{{ route('register') }}">
+                         ¿No tienes una cuenta? Regístrate aquí
+                     </a>
+                 </div>
+ 
+             </form>
+         </div>
+     </div>
+ @endsection
+ 
 
-                <div class="flex items-center justify-between">
-                    <button
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit">
-                        Iniciar sesión
-                    </button>
-                    {{-- <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                       href="{{ route('password.request') }}">
-                        ¿Olvidaste tu contraseña?
-                    </a> --}}
-                </div>
-            </form>
-        </div>
-    </div>
-@endsection
 
+    
