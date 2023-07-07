2b7b37a1e585009b3042ba807402b797f1029741
educarmas <edcarrasmar@gmail.com>
2023-07-07 13:25:46 +0200

    avances estilo registro estrella

--- html/resources/views/auth/register-controller.blade.php
+++ html/resources/views/auth/register-controller.blade.php
@@ -1,22 +1,23 @@
 @extends('layouts.app') 
 
 @section('content')
-<div class="md:flex md:justify-center md:gap-10 md:items-center ">
+<div class="md:flex md:justify-center md:w-6-12 md:items-center">
     <!-- <div class="md:w-6/12 p-5">
         <img src="{{ asset('img/registrar.jpg') }}" alt="Imagen registro de usuarios" >
     </div> -->
-        <h1>Crear perfil</h1>
-        <div class="md:w-6/12 bg-white p-6 rounded-lg shadow-xl" style="bg-green-400 bg-opacity-80 p-6; width: 758px; height: 111.5px; font-style: Urbanist">
+        <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+        <h1>Crear un perfil Estrella</h1>
         <form action="{{ route('register') }}" method="POST" novalidate>
             @csrf
-            <div class="md:w-6/12 bg-green-400 bg-opacity-80 p-6 rounded-lg shadow-xl" style="width: 758px; height: 111.5px;">
+            <div class="md:w-6/12 bg-white"">
+            <label for="name" class="mb-2 block text-gray-500">
                 Nombre
                 </label>
                 <input 
                     id="name"
                     name="name"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('name') border-red-500 @enderror bg-green-100"
                     value="{{ old('name') }}"
                 />
                 @error('name')
@@ -24,144 +25,148 @@
                 @enderror
             </div>
 
-            <div class="md:w-6/12 bg-green-400 bg-opacity-80 p-6 rounded-lg shadow-xl" style="width: 758px; height: 111.5px;">
-                <label for="last_name" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+                <label for="last_name" class="mb-2 block text-gray-500 font-bold ">
                     Apellidos
                 </label>
                 <input 
                     id="last_name"
                     name="last_name"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('apellidos') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('apellidos') border-red-500 @enderror bg-green-100"
                     value="{{ old('lastname') }}"
             />
                 @error('last_name')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>
-            <div class="md:w-8/16 bg-green-400 bg-opacity-80 p-6 rounded-lg shadow-xl"">
-                <label for="nationality" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+                <label for="nationality" class="mb-2 block text-gray-500 font-bold">
                     Nacionalidad
                 </label>
                 <input 
                     id="nationality"
                     name="nationality"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('email') border-red-500 @enderror bg-green-100"
                     value="{{ old('nationality') }}"
                 />
                 @error('nacionalidad')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>
-            <div class="md:w-6/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: white; width: 758px; height: 111.5px;">
-                <label for="date" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+                <label for="date" class="mb-2 block text-gray-500 font-bold">
                     Fecha de Nacimiento
                 </label>
                 <input 
                     id="date"
                     name="date"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('date') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('date') border-red-500 @enderror bg-green-100"
                 />
                 @error('date')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>
-            <div class="md:w-6/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: white; width: 758px; height: 111.5px;">
-                <label for="height" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+                <label for="height" class="mb-2 block text-gray-500 font-bold">
                     Altura
                 </label>
                 <input 
                     id="height"
                     name="height"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('height') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('height') border-red-500 @enderror bg-green-100"
                 />
                 @error('height')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>      
-            <div class="md:w-6/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: white; width: 758px; height: 111.5px;">
-                <label for="weight" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+                <label for="weight" class="mb-2 block text-gray-500 font-bold">
                     Peso
                 </label>
                 <input 
                     id="weight"
                     name="weight"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('weight') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('weight') border-red-500 @enderror bg-green-100"
                 />
                 @error('weight')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>  
-            <div class="md:w-6/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: white; width: 758px; height: 111.5px;">
-                <label for="eyes_color" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+                <label for="eyes_color" class="mb-2 block text-gray-500 font-bold">
                     Color de ojos
                 </label>
                 <input 
                     id="eyes_color"
                     name="eyes_color"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('eyes_color') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('eyes_color') border-red-500 @enderror bg-green-100"
                 />
                 @error('eyes_color')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>  
             
-            <div class="md:w-6/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: white; width: 758px; height: 111.5px;">
-                <label for="hair_color" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+                <label for="hair_color" class="mb-2 block text-gray-500 font-bold">
                     Color de pelo
                 </label>
                 <input 
                     id="hair_color"
                     name="hair_color"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('hair_color') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('hair_color') border-red-500 @enderror bg-green-100"
                 />
                 @error('hair_color')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>  
-            <div class="md:w-6/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: white; width: 758px; height: 111.5px;">
-                <label for="aditional_info" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+                <label for="aditional_info" class="mb-2 block text-gray-500 font-bold">
                     Info. Adicional
                 </label>
                 <input 
                     id="aditional_info"
                     name="aditional_info"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('aditional_info') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('aditional_info') border-red-500 @enderror bg-green-100"
                 />
                 @error('aditional_info')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>  
-            <div class="md:w-6/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: white; width: 758px; height: 111.5px;">
-                <label for="artistic_skills" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 md:flex md:justify-center md:items-center bg-white p-6">
+                <label for="artistic_skills" class="mb-2 block text-gray-500 font-bold">
                     Habilidades Artísticas
                 </label>
                 <input 
                     id="artistic_skills"
                     name="artistic_skills"
                     type="text"
-                    class="border p-3 w-full rounded-lg @error('artistic_skills') border-red-500 @enderror"
+                    class="border-green-300 p-6 w-full rounded-lg @error('artistic_skills') border-red-500 @enderror bg-green-100"
                 />
                 @error('artistic_skills')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div> 
 <!-- APARTADO FOTO / VIDEO / VIDEO BOOK - BUSCAR PARA SUBIR AL PERFIL -->
+
+
+                <p>Foto / Video / VideoBook de Perfil</p>
+
             <input
             type="submit"
             value="Publicar"
-            class="bg-black hover:bg-gray-900 transition-colors cursor-pointer font-bold w-full p-3 text-white rounded-lg"
+            class="md:w-6/12 bg-black hover:bg-gray-900 transition-colors cursor-pointer font-bold w-full p-3 text-white rounded-lg"
             />
 
 
 </form>
 </div>
 </div>
-@endsection -->
+@endsection 
--- html/resources/views/components/register.blade.php
+++ html/resources/views/components/register.blade.php
@@ -1,25 +1,20 @@
-<!-- <div class="md:flex md:justify-center md:gap-10 md:items-center ">
-    <div class="md:w-6/12 p-5">
+    <div class="md:flex md:justify-center md:w-6-12 md:items-center">
+    <!-- <div class="md:w-6/12 p-5">
         <img src="{{ asset('img/registrar.jpg') }}" alt="Imagen registro de usuarios" >
     </div> -->
-
-    <div class="md:flex md:justify-center md:gap-10 md:items-center ">
-    <div class="md:w-6/12 p-5">
-        <img src="{{ asset('img/registrar.jpg') }}" alt="Imagen registro de usuarios" >
-    </div>
-
-    <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
+        <div class="md:w-6/12 bg-white p-6">
+        <h1>Crear un perfil Estrella</h1>
         <form action="{{ route('register') }}" method="POST" novalidate>
             @csrf
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
+            <div class="md:w-6/12 bg-white"">
+            <label for="name" class="mb-2 block text-gray-500">
                 Nombre
                 </label>
                 <input 
                     id="name"
                     name="name"
                     type="text"
-                    placeholder="Nombre"
-                    class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('name') border-red-500 @enderror bg-green-100"
                     value="{{ old('name') }}"
                 />
                 @error('name')
@@ -27,249 +22,141 @@
                 @enderror
             </div>
 
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-                <label for="last_name" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 bg-white">
+                <label for="last_name" class="mb-2 block text-gray-500 font-bold ">
                     Apellidos
                 </label>
                 <input 
                     id="last_name"
                     name="last_name"
                     type="text"
-                    placeholder="Apellidos"
-                    class="border p-3 w-full rounded-lg @error('apellidos') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('apellidos') border-red-500 @enderror bg-green-100"
                     value="{{ old('lastname') }}"
-                />
-
+            />
                 @error('last_name')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>
-
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-                <label for="nationality" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 bg-white">
+                <label for="nationality" class="mb-2 block text-gray-500 font-bold">
                     Nacionalidad
                 </label>
                 <input 
                     id="nationality"
                     name="nationality"
                     type="text"
-                    placeholder="Nacionalidad"
-                    class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('email') border-red-500 @enderror bg-green-100"
                     value="{{ old('nationality') }}"
                 />
                 @error('nacionalidad')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>
-
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-                <label for="date" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 bg-white">
+                <label for="date" class="mb-2 block text-gray-500 font-bold">
                     Fecha de Nacimiento
                 </label>
                 <input 
                     id="date"
                     name="date"
                     type="text"
-                    placeholder="Fecha de Nacimiento"
-                    class="border p-3 w-full rounded-lg @error('date') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('date') border-red-500 @enderror bg-green-100"
                 />
                 @error('date')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>
-
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-                <label for="height" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 bg-white">
+                <label for="height" class="mb-2 block text-gray-500 font-bold">
                     Altura
                 </label>
                 <input 
                     id="height"
                     name="height"
                     type="text"
-                    placeholder="Altura"
-                    class="border p-3 w-full rounded-lg @error('height') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('height') border-red-500 @enderror bg-green-100"
                 />
                 @error('height')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>      
-
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-                <label for="weight" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 bg-white">
+                <label for="weight" class="mb-2 block text-gray-500 font-bold">
                     Peso
                 </label>
                 <input 
                     id="weight"
                     name="weight"
                     type="text"
-                    placeholder="Peso"
-                    class="border p-3 w-full rounded-lg @error('weight') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('weight') border-red-500 @enderror bg-green-100"
                 />
                 @error('weight')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>  
-
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-                <label for="eyes_color" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 bg-white">
+                <label for="eyes_color" class="mb-2 block text-gray-500 font-bold">
                     Color de ojos
                 </label>
                 <input 
                     id="eyes_color"
                     name="eyes_color"
                     type="text"
-                    placeholder="Color de ojos"
-                    class="border p-3 w-full rounded-lg @error('eyes_color') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('eyes_color') border-red-500 @enderror bg-green-100"
                 />
                 @error('eyes_color')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>  
             
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-                <label for="hair_color" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 bg-white">
+                <label for="hair_color" class="mb-2 block text-gray-500 font-bold">
                     Color de pelo
                 </label>
                 <input 
                     id="hair_color"
                     name="hair_color"
                     type="text"
-                    placeholder="Color de pelo"
-                    class="border p-3 w-full rounded-lg @error('hair_color') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('hair_color') border-red-500 @enderror bg-green-100"
                 />
                 @error('hair_color')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>  
-
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-                <label for="aditional_info" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 bg-white">
+                <label for="aditional_info" class="mb-2 block text-gray-500 font-bold">
                     Info. Adicional
                 </label>
                 <input 
                     id="aditional_info"
                     name="aditional_info"
                     type="text"
-                    placeholder="Info. Adicional"
-                    class="border p-3 w-full rounded-lg @error('aditional_info') border-red-500 @enderror"
+                    class="border-green-300 p-2 w-full rounded-lg @error('aditional_info') border-red-500 @enderror bg-green-100"
                 />
                 @error('aditional_info')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div>  
-
-            <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-                <label for="artistic_skills" class="mb-2 block uppercase text-gray-500 font-bold">
+            <div class="md:w-6/12 bg-white">
+                <label for="artistic_skills" class="mb-2 block text-gray-500 font-bold">
                     Habilidades Artísticas
                 </label>
                 <input 
                     id="artistic_skills"
                     name="artistic_skills"
                     type="text"
-                    placeholder="Habilidades Artísticas"
-                    class="border p-3 w-full rounded-lg @error('artistic_skills') border-red-500 @enderror"
+                    class="border-green-300 p-6 w-full rounded-lg @error('artistic_skills') border-red-500 @enderror bg-green-100"
                 />
                 @error('artistic_skills')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
                 @enderror
             </div> 
-
+<!-- APARTADO FOTO / VIDEO / VIDEO BOOK - BUSCAR PARA SUBIR AL PERFIL -->
             <input
-                type="submit"
-                value="Crear Cuenta"
-                class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+            type="submit"
+            value="Publicar"
+            class="md:w-6/12 bg-black hover:bg-gray-900 transition-colors cursor-pointer font-bold w-full p-3 text-white rounded-lg"
             />
-
-</form>
-</div>
-</div>    <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl" style="background-color: rgba(133, 200, 178, 0.8);">
-        <form action="{{ route('register') }}" method="POST" novalidate>
-            @csrf
-            <div class="mb-5">
-                Nombre
-                </label>
-                <input 
-                    id="name"
-                    name="name"
-                    type="text"
-                    placeholder="Tu Nombre"
-                    class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
-                    value="{{ old('name') }}"
-                />
-
-                @error('name')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
-            </div>
-            <div class="mb-5">
-                <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
-                    Username
-                </label>
-                <input 
-                    id="username"
-                    name="username"
-                    type="text"
-                    placeholder="Tu Nombre de Usuario"
-                    class="border p-3 w-full rounded-lg @error('username') border-red-500 @enderror"
-                    value="{{ old('username') }}"
-                />
-
-                @error('username')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
-            </div>
-
-            <div class="mb-5">
-                <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
-                    Email
-                </label>
-                <input 
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Tu Email de Registro"
-                    class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
-                    value="{{ old('email') }}"
-                />
-                @error('email')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
-            </div>
-            <div class="mb-5">
-                <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
-                    Password
-                </label>
-                <input 
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder="Password de Registro"
-                    class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"
-                />
-                @error('password')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
-            </div>
-
-            <div class="mb-5">
-                <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
-                    Repetir Password
-                </label>
-                <input 
-                    id="password_confirmation"
-                    name="password_confirmation"
-                    type="password"
-                    placeholder="Repite tu Password"
-                    class="border p-3 w-full rounded-lg"
-                />
-            </div>
-
-            <input
-                type="submit"
-                value="Crear Cuenta"
-                class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
-            />
-
 </form>
 </div>
 </div>
