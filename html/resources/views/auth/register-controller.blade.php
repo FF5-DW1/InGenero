e59dc52651f809ca96f56e122b1f663f66c29ac3
educarmas <edcarrasmar@gmail.com>
2023-07-10 11:53:15 +0200

    modificaciones imagenes

--- html/resources/views/auth/register-controller.blade.php
+++ html/resources/views/auth/register-controller.blade.php
@@ -3,107 +3,79 @@
 @section('content')
 
 <!-- ENCABEZADO IMAGEN ADMIN -->
-
-<div class="py-10 mt-10 fixed top-0 bg-cover bg-center lg:h-800 w-1920 relative">
-    <img src="img/imagenhero.jpg" alt="" class="w-full h-80 z-10 object-cover">
-    <div class="absolute bottom-0 right-0 p-4">
-        <h1 class="text-2xl font-extrabold leading-none text-white lg:text-4xl">Administrador@</h1>
+<div>
+    <div class="py-10 mt-10 sticky top-0 bg-cover bg-center lg:h-800 w-1920">
+        <div class="absolute w-full bg-no-repeat overflow-hidden">
+            <img src="{{ asset('img/encabezado1.jpg') }}" alt="" class="w-full object-fill h-48 w-">
+        </div>
+        <div class="text-right m-5 lg:my-12 relative top-20 lg:w-3/4 lg:text-left max-w-full">
+            <h1 class="m-4 text-2xl font-extrabold leading-none text-white lg:text-4xl py-4 sm:px-8 lg:px-20">Administrador@</h1>
+        </div>
     </div>
-</div>
 
-<div class="md:flex md:justify-center md:items-center">
-
-    <div class="md:w-6/12 lg:w-4/12 bg-white p-6">
+    <div class="md:w-6/12 lg:w-4/12 relative bg-white p-6">
         <h1 class="text-2xl mb-4">Crear un perfil Estrella</h1>
         <form action="{{ route('register') }}" method="POST" novalidate>
             @csrf
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Nombre</label>
-                <input id="name" name="name" type="text" class="border-green-300 p-2 w-full rounded-lg @error('name') border-red-500 @enderror bg-green-100" value="{{ old('name') }}" />
-                @error('name')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
+                <input id="name" name="name" type="text" class="border-green-300 p-2 w-full rounded-lg @error('name') border-red-500 @enderror bg-green-100"/>
             </div>
 
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Apellidos</label>
-                <input id="last_name" name="last_name" type="text" class="border-green-300 p-2 w-full rounded-lg @error('apellidos') border-red-500 @enderror bg-green-100" value="{{ old('lastname') }}" />
-                @error('last_name')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
+                <input id="last_name" name="last_name" type="text" class="border-green-300 p-2 w-full rounded-lg @error('apellidos') border-red-500 @enderror bg-green-100"/>
             </div>
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Nacionalidad</label>
-                <input id="nationality" name="nationality" type="text" class="border-green-300 p-2 w-full rounded-lg @error('email') border-red-500 @enderror bg-green-100" value="{{ old('nationality') }}" />
-                @error('nacionalidad')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
+                <input id="nationality" name="nationality" type="text" class="border-green-300 p-2 w-full rounded-lg @error('email') border-red-500 @enderror bg-green-100" />
             </div>
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Fecha de Nacimiento</label>
                 <input id="date" name="date" type="text" class="border-green-300 p-2 w-full rounded-lg @error('date') border-red-500 @enderror bg-green-100" />
-                @error('date')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
             </div>
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Altura</label>
                 <input id="height" name="height" type="text" class="border-green-300 p-2 w-full rounded-lg @error('height') border-red-500 @enderror bg-green-100" />
-                @error('height')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
             </div>
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Peso</label>
                 <input id="weight" name="weight" type="text" class="border-green-300 p-2 w-full rounded-lg @error('weight') border-red-500 @enderror bg-green-100" />
-                @error('weight')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
             </div>
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Color de ojos</label>
                 <input id="eyes_color" name="eyes_color" type="text" class="border-green-300 p-2 w-full rounded-lg @error('eyes_color') border-red-500 @enderror bg-green-100" />
-                @error('eyes_color')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
             </div>
 
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Color de pelo</label>
                 <input id="hair_color" name="hair_color" type="text" class="border-green-300 p-2 w-full rounded-lg @error('hair_color') border-red-500 @enderror bg-green-100" />
-                @error('hair_color')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
             </div>
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Info. Adicional</label>
                 <input id="aditional_info" name="aditional_info" type="text" class="border-green-300 p-2 w-full rounded-lg @error('aditional_info') border-red-500 @enderror bg-green-100" />
-                @error('aditional_info')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
             </div>
             <div class="mb-6">
                 <label for="name" class="block mb-2 text-gray-500">Habilidades artísticas</label>
                 <input id="artistic_skills" name="artistic_skills" type="text" class="border-green-300 p-6 w-full rounded-lg @error('artistic_skills') border-red-500 @enderror bg-green-100" />
-                @error('artistic_skills')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
             </div>
             <!-- APARTADO FOTO / VIDEO / VIDEO BOOK - BUSCAR PARA SUBIR AL PERFIL -->
-            <p>Foto / Video / VideoBook de Perfil</p>
-
-            <button type="submit" class="bg-gray-400 hover:bg-gray-500 text-black font-bold py-2 px-4 rounded">
-                Buscar
-            </button>
+            <div>
+                <p>Foto / Video / VideoBook de Perfil</p>
+                <button type="submit" class="bg-gray-400 hover:bg-gray-500 text-black font-bold py-2 px-4 rounded">
+                    Buscar
+                </button>
+            </div>
 
             <input type="submit" value="Publicar" class="md:w-6/12 bg-black hover:bg-gray-900 transition-colors cursor-pointer font-bold w-full p-3 text-white rounded-lg" />
 
         </form>
     </div>
+
+    <footer class="py-10 mt-10 fixed top-0 bg-cover bg-center lg:h-800 w-full relative bg-green-100 flex flex-col items-center justify-center">
+        <img src="img/logo 1.png" class="logo w-36 h-auto">
+        <img src="img/firma_distrito_centro_madrid_pantoneazul 1.png" class="logo w-36 h-auto">
+        <p class="text-center">In Género-Todos los derechos reservados @2023</p>
+    </footer>
 </div>
-<footer class="py-10 mt-10 fixed top-0 bg-cover bg-center lg:h-800 w-full relative bg-green-100 flex flex-col items-center justify-center">
-    <img src="img/logo 1.png" class="logo w-36 h-auto">
-    <img src="img/firma_distrito_centro_madrid_pantoneazul 1.png" class="logo w-36 h-auto">
-    <p class="text-center">In Género-Todos los derechos reservados @2023</p>
-</footer>
 @endsection
--- html/resources/views/components/registermadrina.blade.php
+++ html/resources/views/components/registermadrina.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 
 @section('content')
 
 <!-- ENCABEZADO IMAGEN ADMIN -->
-    <img src="app/public/img/imagenhero.jpg" alt="Administrador">
+<img src="app/public/img/imagenhero.jpg" alt="Administrador">
 
 
 <div class="md:flex md:justify-center md:items-center">
@@ -14,64 +14,36 @@
             @csrf
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Nombre</label>
-                <input 
-                    id="name"
-                    name="name"
-                    type="text"
-                    class="border-green-300 p-2 w-full rounded-lg @error('name') border-red-500 @enderror bg-green-100"
-                    value="{{ old('name') }}"
-                />
-                @error('name')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
+                <input id="name" name="name" type="text" class="border-green-300 p-2 w-full rounded-lg @error('name') border-red-500 @enderror bg-green-100" value="{{ old('name') }}" />
             </div>
 
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-gray-500">Apellidos</label>
-                <input 
-                    id="last_name"
-                    name="last_name"
-                    type="text"
-                    class="border-green-300 p-2 w-full rounded-lg @error('apellidos') border-red-500 @enderror bg-green-100"
-                    value="{{ old('lastname') }}"
-            />
-                @error('last_name')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
+                <input id="last_name" name="last_name" type="text" class="border-green-300 p-2 w-full rounded-lg @error('apellidos') border-red-500 @enderror bg-green-100" value="{{ old('lastname') }}" />
             </div>
             <div class="mb-4">
 
-            <div class="mb-6">
-                <label for="name" class="block mb-2 text-gray-500">Descripción/label>
-                <input 
-                    id="description"
-                    name="description"
-                    type="text"
-                    class="border-green-300 p-12 w-full rounded-lg @error('artistic_skills') border-red-500 @enderror bg-green-100"
-                />
-                @error('description')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }} </p>
-                @enderror
-            </div> 
-<!-- APARTADO FOTO / VIDEO / VIDEO BOOK - BUSCAR PARA SUBIR AL PERFIL -->
-            <p>Foto / Video / VideoBook de Perfil</p>
+                <div class="mb-6">
+                    <label for="name" class="block mb-2 text-gray-500">Descripción/label>
+                        <input id="description" name="description" type="text" class="border-green-300 p-12 w-full rounded-lg @error('artistic_skills') border-red-500 @enderror bg-green-100" />
+                </div>
+                <!-- APARTADO FOTO / VIDEO / VIDEO BOOK - BUSCAR PARA SUBIR AL PERFIL -->
+                <div>
+                    <p>Foto / Video / VideoBook de Perfil</p>
+                </div>
 
-            <button type="submit" class="bg-gray-400 hover:bg-gray-500 text-black font-bold py-2 px-4 rounded">
+                <button type="submit" class="bg-gray-400 hover:bg-gray-500 text-black font-bold py-2 px-4 rounded">
                     Buscar
-            </button>
+                </button>
 
-            <input
-            type="submit"
-            value="Publicar"
-            class="md:w-6/12 bg-black hover:bg-gray-900 transition-colors cursor-pointer font-bold w-full p-3 text-white rounded-lg"
-            />
-<!-- FOOTER -->
+                <input type="submit" value="Publicar" class="md:w-6/12 bg-black hover:bg-gray-900 transition-colors cursor-pointer font-bold w-full p-3 text-white rounded-lg" />
+        </form>
+    </div>
+</div>
 
-            <footer class="mb-6" bg-green-200>
-                <p>In Género-Todos los derechos reservados @2023</p>
-            <img src="html/app/public/img/logo1.png">
-            </footer>
-</form>
-</div>
-</div>
-@endsection 
+<footer class="py-10 mt-10 fixed top-0 bg-cover bg-center lg:h-800 w-full relative bg-green-100 flex flex-col items-center justify-center">
+    <img src="img/logo 1.png" class="logo w-36 h-auto">
+    <img src="img/firma_distrito_centro_madrid_pantoneazul 1.png" class="logo w-36 h-auto">
+    <p class="text-center">In Género-Todos los derechos reservados @2023</p>
+</footer>
+@endsection
