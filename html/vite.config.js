f9b7e38c2fd2b2ffb43e702878575c6b9c7fe322
Pablo Caballero <pablo.caballero.marin@outlook.es>
2023-07-13 01:55:30 +0200

    frontend y views casi terminadas, todo listo para empezar con el emparejamiento con el backend

--- html/vite.config.js
+++ html/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'public/css/style.css',
+        'public/css/fonts.css',
+        'public/css/bootstrap.css',
+        'public/js/script.js',
+        'public/js/core.min.js'
+      ],
+      refresh: true,
+    }),
+  ],
 });
