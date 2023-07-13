741c97bfcfbdad05e1ae0ec764485c2d791d8b7e
educarmas <edcarrasmar@gmail.com>
2023-07-13 11:37:41 +0200

    modificación footer

--- html/resources/views/auth/gestionadmin.blade.php
+++ html/resources/views/auth/gestionadmin.blade.php
@@ -142,11 +142,32 @@
             </table>
         </div>
 
+        <footer class="bg-green-200 text-zinc w-full flex flex-col justify-center py-10">
+            <div class="container mx-auto flex flex-wrap content-center justify-between">
+                <div class="w-full md:w-1/4 mb-6">
+                    <img src="img\logoencabezado.png" alt="Logo" class="logo w-38 h-12">
+                </div>
+                <div class="w-full md:w-1/5 mb-10">
+                    <h4 class="text-lg font-bold">Redes Sociales</h4>
+                    <div class="w-full md:w-1/2 flex mb-5">
+                        <img src="img\TwitterLogo.png" alt="icono" class="icono w-10 h-10">
+                        <img src="img\FacebookLogo.png" alt="icono" class="icono mx-1 w-10 h-10">
+                        <img src="img\InstagramLogo.png" alt="icono" class="icono mx-1 w-10 h-10">
+                        <img src="img\YoutubeLogo.png" alt="icono" class="icono mx-1 w-10 h-10">
+                    </div>
+                </div>
+                <div class="w-full md:w-1/4 mb-6">
+                    <img src="img\firmamadrid.png" alt="Logo2" class="logo2 w-78 h-20 w-full">
+                </div>
+            </div>
+            <div class="container mx-auto mt-auto">
+                <div class="w-full text-center">
+                    <div class="IngeneroTodosLosDerechosReservados2023 text-black text-[18px] font-normal">
+                        InGenero - todos los derechos reservados @2023
+                    </div>
+                </div>
+            </div>
 
-        <footer class="py-10 mt-10 top-0 bg-cover bg-center lg:h-800 w-full relative bg-green-100 flex flex-col items-center justify-center">
-            <img src="img/logo 1.png" class="logo w-36 h-auto">
-            <img src="img/firma_distrito_centro_madrid_pantoneazul 1.png" class="logo w-36 h-auto">
-            <p class="text-center">In Género-Todos los derechos reservados @2023</p>
         </footer>
     </div>
     @endsection
